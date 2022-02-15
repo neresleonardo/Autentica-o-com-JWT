73b407cec3b336b94167a673a11e90851357fca4
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-15 02:22:35 -0300

    ✔Configuring axios client

--- auth/contexts/AuthContext.tsx
+++ auth/contexts/AuthContext.tsx
@@ -1,17 +1,18 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCrendemtials = {
     email: string,
     password: string
 }
 
-type AuthContextData ={
+type AuthContextData = {
     signIn(credentials: SignInCrendemtials): Promise<void>;
     isAuthenticated: boolean;
 }
 
 type AuthProviderProps = {
-   children: ReactNode; 
+    children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -20,14 +21,26 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
     const isAuthenticated = false;
 
-   async function signIn({email, password}: SignInCrendemtials){
-        console.log({email, password});
-    }
+    async function signIn({ email, password }: SignInCrendemtials) {
+        //chamada de autenticação 
+
+        try {
+            const response = await api.post('sessions', {
+                email,
+                password
+            })
+
+            console.log(response.data)
+        } catch (err) {
+            console.log(err);
+            
+        }
+}
 
     return (
-        
-        <AuthContext.Provider value={{signIn, isAuthenticated }}>
-                {children}
+
+        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+            {children}
         </AuthContext.Provider>
-    )       
+    )
 }
