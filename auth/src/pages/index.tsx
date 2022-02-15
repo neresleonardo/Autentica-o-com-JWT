5a5011e906a3a7ceb138639e544de281b2ce0b0c
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-15 18:04:34 -0300

    💎Recuperando token no server-side

--- auth/src/pages/index.tsx
+++ auth/src/pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import { FormEvent, useContext, useState } from 'react'
+import { parseCookies } from 'nookies'
 import { AuthContext } from '../../contexts/AuthContext';
 
 
@@ -33,3 +34,23 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }
+  
+
+  return{
+    props: {}
+
+  }
+}
